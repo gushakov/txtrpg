d01b70309059e363fb85ffb2320ca18edc4f00e4
gushakov <gushakov@yahoo.com>
2014-02-23 23:08:12 +0100

    maven antlr4 plugin for command parsing

--- src/test/java/com/github/txtrpg/antlr4/CommandParserTest.java
+++ src/test/java/com/github/txtrpg/antlr4/CommandParserTest.java
@@ -1,7 +1,13 @@
 package com.github.txtrpg.antlr4;
 
+import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.UnbufferedCharStream;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
 
 import java.io.StringReader;
 
@@ -10,11 +16,28 @@ import java.io.StringReader;
  */
 public class CommandParserTest {
 
+    public class CommandListener extends SimpleBaseListener {
+
+        @Override
+        public void enterCommand(@NotNull SimpleParser.CommandContext ctx) {
+            System.out.println(ctx.getText());
+            super.enterCommand(ctx);
+        }
+    }
+
+    @Test
     public void testParse() throws Exception {
-        SimpleLexer lexer = new SimpleLexer(new UnbufferedCharStream(new StringReader("n")));
+        SimpleLexer lexer = new SimpleLexer(new ANTLRInputStream("look"));
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         SimpleParser parser = new SimpleParser(tokens);
-        parser.move();
+
+        ParseTreeWalker walker = new ParseTreeWalker();
+
+       walker.walk(new CommandListener(), parser.command());
+
+
+
+
     }
 
 }
